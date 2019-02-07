3cb72a6103cf3d99270c37d7ec3c12b00469f472
Harry Braviner <harry@layer6.ai>
2019-02-07 16:16:50 -0500

    Factored out some code

---
+++ src/main/java/braviner/gp_experiments/GPUtils.java
@@ -0,0 +1,151 @@
+package braviner.gp_experiments;
+
+import com.github.fommil.netlib.BLAS;
+import com.github.fommil.netlib.LAPACK;
+import org.netlib.util.intW;
+
+import java.util.Arrays;
+import java.util.function.BiFunction;
+
+public class GPUtils {
+
+
+    /**
+     * Wrapper class to allow me to return two arrays.
+     */
+    static class PosteriorResults {
+        private double[] means;
+        private double[] variances;
+
+        PosteriorResults(double[] means, double[] variances) {
+            this.means = means;
+            this.variances = variances;
+        }
+
+        public double[] getMeans() {
+            return means;
+        }
+
+        public double[] getVariances() {
+            return variances;
+        }
+    }
+
+    /**
+     * Generate a matrix with elements K[i ,j] = kernel(x1[i], x2[j])
+     * in column-major order.
+     * @param x1 Input vector.
+     * @param x2 Input vector.
+     * @return Matrix in column-major order.
+     */
+    static double[] generateKernelMatrix(BiFunction<Double, Double, Double> kernelFunction,
+                                         double[] x1, double[] x2) {
+        int N1 = x1.length;
+        int N2 = x2.length;
+        double[] kernelMatrix = new double[N1*N2];
+
+        for (int i=0; i < N1; i++) {
+            for (int j=0; j < N2; j++) {
+                kernelMatrix[i + j*N1] = kernelFunction.apply(x1[i], x2[j]);
+            }
+        }
+
+        return kernelMatrix;
+    }
+
+    /**
+     * Returns (by mutating arrays passed in) the posterior means and variances
+     * for new x-values.
+     *
+     * @param originalXValues x-values of the original observations.
+     * @param originalYValues y-values of the original observations.
+     * @param newXValues x-values we want predictions for.
+     * @return Posterior means and variances at each point in newXValues.
+     */
+    static PosteriorResults getPredictionsAndStddevs(double[] originalXValues, double[] originalYValues,
+                                                     BiFunction<Double, Double, Double> kernelFunction,
+                                                     double[] newXValues) {
+        double[] kernelMatrix = generateKernelMatrix(kernelFunction, originalXValues, originalXValues);
+        return getPredictionsAndStddevs(originalXValues, originalYValues, kernelFunction, newXValues);
+    }
+
+
+    /**
+     * Returns (by mutating arrays passed in) the posterior means and variances
+     * for new x-values.
+     *
+     * @param kernelMatrix Matrix of the prior covariances between y-values. Column-major order.
+     * @param originalXValues x-values of the original observations.
+     * @param originalYValues y-values of the original observations.
+     * @param newXValues x-values we want predictions for.
+     * @return Posterior means and variances at each point in newXValues.
+     */
+    static PosteriorResults getPredictionsAndStddevs(double[] kernelMatrix, double[] originalXValues,
+                                                     double[] originalYValues,
+                                                     BiFunction<Double, Double, Double> kernelFunction,
+                                                     double[] newXValues) {
+
+        LAPACK lapack = LAPACK.getInstance();
+        BLAS blas = BLAS.getInstance();
+
+        // Pull out N and N* and check that dimensions all make sense.
+        int N = (int)Math.sqrt(kernelMatrix.length);
+        if (N*N != kernelMatrix.length) {
+            throw new IllegalArgumentException("kernelMatrix was not square.");
+        }
+        if (originalXValues.length != N) {
+            throw new IllegalArgumentException("originalXValues size does not match kernelMatrix.");
+        }
+        if (originalYValues.length != N) {
+            throw new IllegalArgumentException("originalYValues size does not match kernelMatrix.");
+        }
+
+        int N_star = newXValues.length;
+
+        // Construct the matrix of covariances of new x-value and original
+        // (stored in column-major order)
+        double[] matrixKStar = new double[N*N_star];
+        for (int i=0; i < N; i++) {
+            for (int j=0; j < N_star; j++) {
+                matrixKStar[i + j*N] = kernelFunction.apply(originalXValues[i], newXValues[j]);
+            }
+        }
+
+        double[] kernelMatrixClone = kernelMatrix.clone();
+        int[] ipiv = new int[N];
+
+        // We'll package getting the posterior mean and the posterior variance into one calculation
+        double[] rhs = Arrays.copyOf(matrixKStar, N*(N_star + 1));
+        for (int i=0; i < N; i++) {
+            rhs[N*N_star + i] = originalYValues[i];
+        }
+
+        int lWork = N;
+        double[] work = new double[N];
+        intW info = new intW(0);
+        // This populates originalYValuesClone with the solutions to the linear equation K x = [K_*, f]
+        lapack.dsysv("U", N, (N_star + 1), kernelMatrixClone, N, ipiv, rhs, N_star, work, lWork, info);
+
+        double[] outputMemory = new double[N_star*(N_star + 1)];
+        // This populates outputMemory with K_*^T acting on the K^(-1) [K_*, f]
+        blas.dgemm("T", "N", N_star, (N_star + 1), N, 1.0, matrixKStar, N, rhs, N, 0.0, outputMemory, N_star);
+
+        double[] posteriorMeans = new double[N_star];
+        for (int i=0; i<N_star; i++) {
+            posteriorMeans[i] = outputMemory[N_star*N_star + i];
+        }
+
+        // Construct the matrix of prior covariances of the new points,
+        // and subtract K_*^T K^-1 K_* at the same time.
+        double[] posteriorVariances = new double[N_star*N_star];
+        for (int i=0; i < N_star; i++) {
+            for (int j=0; j < N_star; j++) {
+                posteriorVariances[i + j*N_star] = kernelFunction.apply(newXValues[i], newXValues[j])
+                        - outputMemory[i + j*N_star];
+            }
+        }
+
+        return new PosteriorResults(posteriorMeans, posteriorVariances);
+
+    }
+}
--- src/main/java/braviner/gp_experiments/ZeroNoiseExample.java
+++ src/main/java/braviner/gp_experiments/ZeroNoiseExample.java
@@ -3,11 +3,8 @@ package braviner.gp_experiments;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Random;
 import com.github.fommil.netlib.LAPACK;
-import com.github.fommil.netlib.BLAS;
-import org.netlib.util.intW;
 
 public class ZeroNoiseExample {
 
@@ -33,94 +30,22 @@ public class ZeroNoiseExample {
      */
     static double kernelFunction(double x1, double x2) {
         return Math.exp(-0.5 * (x1 - x2)*(x1 - x2));
-//        throw new RuntimeException("Not implemented");
-    }
-
-    static class PosteriorResults {
-        private double[] means;
-        private double[] variances;
-
-        PosteriorResults(double[] means, double[] variances) {
-            this.means = means;
-            this.variances = variances;
-        }
     }
 
     /**
-     * Returns (by mutating arrays passed in) the posterior means and variances
-     * for new x-values.
      *
-     * @param kernelMatrix Matrix of the prior covariances between y-values. Column-major order.
-     * @param originalXValues x-values of the original observations.
-     * @param originalYValues y-values of the original observations.
-     * @param newXValues x-values we want predictions for.
-     * @return Posterior means and variances at each point in newXValues.
+     * @param x_min Start point (inclusive)
+     * @param x_max End point (inclusive)
+     * @param N Number of points to generate (>= 2)
+     * @return Array of evenly spaced points.
      */
-    static PosteriorResults getPredictionsAndStddevs(double[] kernelMatrix, double[] originalXValues,
-                                        double[] originalYValues, double[] newXValues) {
-
-        LAPACK lapack = LAPACK.getInstance();
-        BLAS blas = BLAS.getInstance();
-
-        // Pull out N and N* and check that dimensions all make sense.
-        int N = (int)Math.sqrt(kernelMatrix.length);
-        if (N*N != kernelMatrix.length) {
-            throw new IllegalArgumentException("kernelMatrix was not square.");
-        }
-        if (originalXValues.length != N) {
-            throw new IllegalArgumentException("originalXValues size does not match kernelMatrix.");
-        }
-        if (originalYValues.length != N) {
-            throw new IllegalArgumentException("originalYValues size does not match kernelMatrix.");
-        }
-
-        int N_star = newXValues.length;
-
-        // Construct the matrix of covariances of new x-value and original
-        // (stored in column-major order)
-        double[] matrixKStar = new double[N*N_star];
+    static double[] generateEvenlySpacedPoints(double x_min, double x_max, int N) {
+        double[] output = new double[N];
         for (int i=0; i < N; i++) {
-            for (int j=0; j < N_star; j++) {
-                matrixKStar[i + j*N] = kernelFunction(originalXValues[i], newXValues[j]);
-            }
+            output[i] = x_min + (x_max - x_min) * i / (N - 1.0);
         }
 
-        double[] kernelMatrixClone = kernelMatrix.clone();
-        int[] ipiv = new int[N];
-
-        // We'll package getting the posterior mean and the posterior variance into one calculation
-        double[] rhs = Arrays.copyOf(matrixKStar, N*(N_star + 1));
-        for (int i=0; i < N; i++) {
-            rhs[N*N_star + i] = originalYValues[i];
-        }
-
-        int lWork = N;
-        double[] work = new double[N];
-        intW info = new intW(0);
-        // This populates originalYValuesClone with the solutions to the linear equation K x = [K_*, f]
-        lapack.dsysv("U", N, (N_star + 1), kernelMatrixClone, N, ipiv, rhs, N_star, work, lWork, info);
-
-        double[] outputMemory = new double[N_star*(N_star + 1)];
-        // This populates outputMemory with K_*^T acting on the K^(-1) [K_*, f]
-        blas.dgemm("T", "N", N_star, (N_star + 1), N, 1.0, matrixKStar, N, rhs, N, 0.0, outputMemory, N_star);
-
-        double[] posteriorMeans = new double[N_star];
-        for (int i=0; i<N_star; i++) {
-            posteriorMeans[i] = outputMemory[N_star*N_star + i];
-        }
-
-        // Construct the matrix of prior covariances of the new points,
-        // and subtract K_*^T K^-1 K_* at the same time.
-        double[] posteriorVariances = new double[N_star*N_star];
-        for (int i=0; i < N_star; i++) {
-            for (int j=0; j < N_star; j++) {
-                posteriorVariances[i + j*N_star] = kernelFunction(newXValues[i], newXValues[j])
-                        - outputMemory[i + j*N_star];
-            }
-        }
-
-        return new PosteriorResults(posteriorMeans, posteriorVariances);
-
+        return output;
     }
 
     public static void main(String[] args) throws IOException {
@@ -143,23 +68,16 @@ public class ZeroNoiseExample {
             sampleY[i] = y(samplePoints[i]);
         }
 
-        double[] matrix_K = new double[N_observed*N_observed];
-        for (int i=0; i < N_observed; i++) {
-            // Populate the whole thing since I'll use general matrix routines for now
-            for (int j=0; j < N_observed; j++) {
-                matrix_K[i + j*N_observed] = kernelFunction(samplePoints[i], samplePoints[j]);
-            }
-        }
+        double[] matrix_K = GPUtils.generateKernelMatrix(ZeroNoiseExample::kernelFunction, samplePoints, samplePoints);
 
         // Generate an even grid of many points that we'll use to plot the function.
         int N_star = 100;
-        double[] evenlySpacedPoints = new double[N_star];
-        for (int i=0; i < N_star; i++) {
-            evenlySpacedPoints[i] = x_min + (x_max - x_min) * i / (N_star - 1.0);
-        }
+        double[] evenlySpacedPoints = generateEvenlySpacedPoints(x_min, x_max, N_star);
 
-        PosteriorResults results = getPredictionsAndStddevs(matrix_K, samplePoints, sampleY, evenlySpacedPoints);
+        GPUtils.PosteriorResults results = GPUtils.getPredictionsAndStddevs(matrix_K, samplePoints, sampleY,
+                ZeroNoiseExample::kernelFunction, evenlySpacedPoints);
 
+        // Write results to a file
         try (BufferedWriter samplePointsWriter = new BufferedWriter(new FileWriter("samplePoints.csv"))) {
             samplePointsWriter.write("x,y\n");
             for (int i=0; i < samplePoints.length; i++) {
@@ -170,7 +88,7 @@ public class ZeroNoiseExample {
         try (BufferedWriter predictionsWriter = new BufferedWriter(new FileWriter("predictions.csv"))) {
             predictionsWriter.write("x,postMean,postVar\n");
             for (int i=0; i < evenlySpacedPoints.length; i++) {
-                predictionsWriter.write(evenlySpacedPoints[i] + "," + results.means[i] + "," + results.variances[i] + "\n");
+                predictionsWriter.write(evenlySpacedPoints[i] + "," + results.getMeans()[i] + "," + results.getVariances()[i] + "\n");
             }
         }
 
