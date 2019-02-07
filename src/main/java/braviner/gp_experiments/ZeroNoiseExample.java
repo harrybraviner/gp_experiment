6b6388c0a29638a7164370dd2305217b528d0311
Harry Braviner <harry@layer6.ai>
2019-02-07 14:01:51 -0500

    Output of posterior mean working

--- src/main/java/braviner/gp_experiments/ZeroNoiseExample.java
+++ src/main/java/braviner/gp_experiments/ZeroNoiseExample.java
@@ -1,7 +1,11 @@
 package braviner.gp_experiments;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Random;
 import com.github.fommil.netlib.LAPACK;
+import com.github.fommil.netlib.BLAS;
 import org.netlib.util.intW;
 
 public class ZeroNoiseExample {
@@ -45,6 +49,10 @@ public class ZeroNoiseExample {
     static void getPredictionsAndStddevs(double[] kernelMatrix, double[] originalXValues,
                                          double[] originalYValues, double[] newXValues,
                                          double[] outMeans, double[] outStddevs) {
+
+        LAPACK lapack = LAPACK.getInstance();
+        BLAS blas = BLAS.getInstance();
+
         // Pull out N and N* and check that dimensions all make sense.
         int N = (int)Math.sqrt(kernelMatrix.length);
         if (N*N != kernelMatrix.length) {
@@ -77,9 +85,21 @@ public class ZeroNoiseExample {
         // FIXME - compute K^-1 (f - 0) using dsysv.
         // FIXME - then act with K_*^T, that will give us the mean.
 
+        double[] kernelMatrixClone = kernelMatrix.clone();
+        int[] ipiv = new int[N];
+        double[] originalYValuesClone = originalYValues.clone();
+        int lWork = N;
+        double[] work = new double[N];
+        intW info = new intW(0);
+        // This populates originalYValuesClone with the solutions to the linear equation K x = f
+        lapack.dsysv("U", N, N, kernelMatrixClone, N, ipiv, originalYValuesClone, N_star, work, lWork, info);
+
+        // This populate muStarOutput with K_*^T acting on the K^(-1) f
+        blas.dgemv("T", N, N_star, 1.0, matrixKStar, N, originalYValuesClone, 1, 0.0, outMeans, 1);
+
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         LAPACK lapack = LAPACK.getInstance();
 
@@ -118,6 +138,20 @@ public class ZeroNoiseExample {
         double[] posteriorStddevs = new double[N_star];
         getPredictionsAndStddevs(matrix_K, samplePoints, sampleY, evenlySpacedPoints,
                 posteriorMeans, posteriorStddevs);
+
+        try (BufferedWriter samplePointsWriter = new BufferedWriter(new FileWriter("samplePoints.csv"))) {
+            samplePointsWriter.write("x,y\n");
+            for (int i=0; i < samplePoints.length; i++) {
+                samplePointsWriter.write(samplePoints[i] + "," + sampleY[i] + "\n");
+            }
+        }
+
+        try (BufferedWriter predictionsWriter = new BufferedWriter(new FileWriter("predictions.csv"))) {
+            predictionsWriter.write("x,postMean\n");
+            for (int i=0; i < evenlySpacedPoints.length; i++) {
+                predictionsWriter.write(evenlySpacedPoints[i] + "," + posteriorMeans[i] + "\n");
+            }
+        }
 
     }
 
